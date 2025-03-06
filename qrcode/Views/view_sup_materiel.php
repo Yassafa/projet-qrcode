31d45d69c1372614e262b54490c83a920b05a28e
Yassafa <yassineessafa1@gmail.com>
2025-03-06 14:33:38 +0100

    Add files via upload

---
+++ qrcode/Views/view_sup_materiel.php
@@ -0,0 +1,16 @@
+<?php require "view_begin.php"; ?>
+
+            <h2>SUPPRIMER UN ÉQUIPEMENT</h2>
+            <form action="?controller=admin&action=del_materiel" method="post" id="form-sup-materiel" autocomplete="off">
+                <div class="autocomplete">
+                    <label>Identifiant : <b>*</b></label><br>
+                    <input id="sup-materiel" class="saisie" type="text" name="id-equipement" placeholder="Id" required/>
+                    <input type="hidden" id="liste-equipements" value="<?= e(json_encode($equipements)) ?>"/>
+                </div>
+
+                <div class="input">
+                    <input class="bouton rouge" type="submit" value="Supprimer"/>
+                </div>
+            </form>
+
+<?php require "view_end.php"; ?>
